d11567c72324f5281d2055718f69d2ff037a8613
Efrén Paolo Centeno Rosas <efren.centeno@utec.edu.pe>
2025-05-13 16:53:47 -0500

    C2

---
+++ src/main/java/com/example/c2_dbp/C2DbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.c2_dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2DbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2DbpApplication.class, args);
+    }
+
+}
